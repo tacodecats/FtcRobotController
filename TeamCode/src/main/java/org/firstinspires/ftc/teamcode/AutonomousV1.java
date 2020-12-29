eee5a3f77954ad6a59df878b124d4f46d4455ce0
) <tacodecats@gmail.com>
2020-12-29 14:55:17 -0600

    Added wobble arm dropping part

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousV1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousV1.java
@@ -344,7 +344,7 @@ public class AutonomousV1 extends LinearOpMode {
     //Steps for four rings
     public void targetZoneC(){
         //Move forward to box C
-        encoderDrive(.8, 125,125);
+        encoderDrive(.8, 123,125);
         dropWobbleGoal();
         sleep(30000);
     }
